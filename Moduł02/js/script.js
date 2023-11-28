9034d6deff02e38902ee48297462ffffc09981d4
rafalek96 <152285139+rafalek96@users.noreply.github.com>
2023-11-28 17:48:50 +0100

    Delete Moduł02 directory

--- Moduł02/js/script.js
+++
@@ -1,8 +0,0 @@
-console.log("Cześć!");
-
-let naglowek = document.querySelector(".naglowek");
-let przycisk = document.querySelector(".przycisk");
-
-przycisk.addEventListener("click", () => {
-    naglowek.remove();
-});
